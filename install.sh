1ef93349648eaaddf069fee06f7574cb646f7637
Adrián Reina Gálvez <adrianreinagalvez@gmail.com>
2026-01-15 23:03:11 +0100

    Actualizar ruta del directorio de dotfiles en el script de instalación

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Directorio del repositorio dotfiles
-DOTFILES_DIR="$HOME/Dev/Src/dotfiles"
+DOTFILES_DIR="$HOME/Dev/Projects/dotfiles"
 
 echo -e "${GREEN}=====================================${NC}"
 echo -e "${GREEN}  Instalación de Dotfiles${NC}"
